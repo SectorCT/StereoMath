08184ab8e103a440d051ccb4a8f5a7aa96b6f19d
dr-programmer <85807185+dr-programmer@users.noreply.github.com>
2024-03-14 12:26:12 +0200

    Working - getting coordinates from qustion

--- backend/firstGPT/firstGPT/request_tests.py
+++ backend/firstGPT/firstGPT/request_tests.py
@@ -1,16 +1,26 @@
+import os
 from openai import OpenAI
-from dotenv import apiKey
-client = OpenAI(api_key=apiKey)
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("api_key")
+le_format = os.getenv("le_format")
+
+client = OpenAI(api_key=api_key)
+
+test = "Дадена е правилна четириъгълна призма ACBDA1B1C1D1 с основен ръб Ab = 3 и околен ръб AA1 = 4. Намерете косинуса на ъгъл между AD1 и BC."
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
-    {"role": "system", "content": "You are a mathematician and you are going to solve stereometry problems."},
-    {"role": "user", "content": "Zadacha"},
-    {"role": "system", "content": "You must give me the coordinates of the 3d figure in a 3d coordinate space. The output must be in JSON file."},
+    {"role": "system", "content": "You are a mathematical assistant and you are going to work on stereometry qustions."},
+    {"role": "user", "content": test},
+    {"role": "user", "content": \
+     "Дай ми координатите на върховете на фигурата в JSON формат и кои двойки точки се свързват в отсечки. Отговори само с JSON файла без нищо друго. На английски език в този формат: {le_format}"},
   ],
-  stream=True
+  max_tokens = 300
+  #stream=True
 )
 
-for chunk in completion:
-  print(chunk.choices[0].delta)
+print(completion.choices[0].message.content, end="")
