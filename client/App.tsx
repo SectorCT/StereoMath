bb8faacf21189941bf0391d1463fb38b14d1860f
Svetoslav47 <svetoslav2806@gmail.com>
2024-03-14 21:18:39 +0200

    rew

--- client/App.tsx
+++ client/App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Camera from "./Camera"
+import Camera from "./CameraPage"
 
 export default function App() {
   return (
---
+++ client/CameraPage.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from 'expo-camera';
+
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={Camera.Constants.Type.back}>
+        {/* Your other components can go here */}
+      </Camera>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+});
