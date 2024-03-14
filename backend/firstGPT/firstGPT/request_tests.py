4eebd57a1e3e13d9d0fe384c94d7da25a77d3cdb
dr-programmer <85807185+dr-programmer@users.noreply.github.com>
2024-03-14 12:34:50 +0200

    beautify

--- backend/firstGPT/firstGPT/request_tests.py
+++ backend/firstGPT/firstGPT/request_tests.py
@@ -11,14 +11,16 @@ client = OpenAI(api_key=api_key)
 
 test = "Дадена е правилна четириъгълна призма ACBDA1B1C1D1 с основен ръб Ab = 3 и околен ръб AA1 = 4. Намерете косинуса на ъгъл между AD1 и BC."
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
+prompt = [
     {"role": "system", "content": "You are a mathematical assistant and you are going to work on stereometry qustions."},
     {"role": "user", "content": test},
     {"role": "user", "content": \
      "Дай ми координатите на върховете на фигурата в JSON формат и кои двойки точки се свързват в отсечки. Отговори само с JSON файла без нищо друго. На английски език в този формат: {le_format}"},
-  ],
+]
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=prompt,
   max_tokens = 300
   #stream=True
 )
