150774bb57197e77d03be32343249bb52c37ab79
Antonio Gotev <68850596+tonigotev@users.noreply.github.com>
2024-03-14 13:57:36 +0200

    frame for the problem solution function in the views.py

--- backend/firstGPT/firstGPT/views.py
+++ backend/firstGPT/firstGPT/views.py
@@ -1,8 +1,41 @@
-import requests
+import requests, os
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from openai import OpenAI
+from dotenv import load_dotenv
+import json
+from openai import OpenAI
+from dotenv import apiKey
+
+load_dotenv()
+client = OpenAI(api_key=apiKey)
+
+@api_view(['POST'])
+@csrf_exempt
+def solution(request):
+    jsonString = request.body.decode('utf-8')
+    jsonBody = json.loads(jsonString)
+    
+    problem = jsonBody['problem']
+    if not problem:
+        return JsonResponse({'success': False, 'message': 'Problem is missing'}, status=400)
+    
+    # The request
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a mathematician and you are going to solve stereometry problems."},
+            {"role": "user", "content": "Zadacha"},
+            {"role": "system", "content": "You must give me the coordinates of the 3d figure in a 3d coordinate space. The output must be in JSON file."},
+        ],
+        stream=True
+        )
+    # ends here
+    
+    
+    
+
 
 
 @api_view(['POST'])
