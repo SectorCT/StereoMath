6ecdf0f38db9fe5b952872f91b3b2ef23d659b5f
DMyashkov <damemyashkov@gmail.com>
2024-03-14 16:23:18 +0200

    Rotation of cube

--- client/mainModel.tsx
+++ client/mainModel.tsx
@@ -1,27 +1,35 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text,TextInput, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { Canvas } from '@react-three/fiber';
 
 function MainModel() {
   return (
-        <Canvas style = {styles.container} shadows >
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} intensity = {2} />
-            <mesh position={[0,0,0]}>
-                <sphereGeometry />
-                <meshStandardMaterial color="orange" />
-            </mesh> 
-        </Canvas>
-  )
+    <Canvas style={styles.container} >
+      <SceneContent />
+    </Canvas>
+  );
+}
+
+function SceneContent() {
+  return (
+    <>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} intensity={2} />
+      <mesh position={[0, 0, 0]} rotation={[(Math.PI / 4), 0, 0]}>
+        <boxGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        width: "100%",
-    },
-    header: {
-        fontSize: 30,
-    }
-})
+  container: {
+    width: '100%',
+  },
+  header: {
+    fontSize: 30,
+  },
+});
 
 export default MainModel;
