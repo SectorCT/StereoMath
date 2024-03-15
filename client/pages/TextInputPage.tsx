4d2d6a5654957f779cde6e581c8020200646dfa0
Svetoslav47 <svetoslav2806@gmail.com>
2024-03-16 01:58:27 +0200

    Update TextInputPage.tsx

--- client/pages/TextInputPage.tsx
+++ client/pages/TextInputPage.tsx
@@ -1,36 +1,58 @@
-import React, { useState } from 'react'
-import { StyleSheet, Text,TextInput, View } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 import { StackNavigationProp } from "@react-navigation/stack";
 import { NavStackParamList } from "../Navigation";
 
 interface Props {
   navigation: StackNavigationProp<NavStackParamList, "GraphicScreen">;
-  route: { };
+  route: {};
 }
-function TextInputPage({navigation, route} : Props) {
-    const [inputValue, onChangeValue] = useState('');
+
+function TextInputPage({ navigation, route }: Props) {
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    console.log("TextInputPage: inputValue:", inputValue);
+  }, [inputValue]);
+
   return (
     <>
-    <View style={styles.main}>
-        <Text style={styles.header}>Photo Stereo</Text> 
-        <TextInput value={inputValue} placeholder='Type your hardest stereometric math problem here...'
-         onChangeText={onChangeValue}/>
-    </View>
+      <View style={styles.main}>
+        <Text style={styles.header}>Photo Stereo</Text>
+        <TextInput 
+          style={styles.input} 
+          value={inputValue} 
+          placeholder='Type your hardest stereometric math problem here...'
+          onChangeText={(text) => {
+            setInputValue(text);
+          }} 
+          multiline
+          textAlignVertical='top'
+        />
+      </View>
     </>
   )
 }
 
 const styles = StyleSheet.create({
-    main: {
-        position: "relative",
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    header: {
-        fontSize: 20,
-    }
+  main: {
+    position: "relative",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    fontSize: 20,
+  },
+  input: {
+    fontSize: 20,
+    width: 300,
+    height: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+    textAlignVertical: 'top', // Adjust the text to start from the top
+  }
 });
 
 export default TextInputPage;
