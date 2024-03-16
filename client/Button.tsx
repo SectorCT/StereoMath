e05190014a4ec8fde4ccc7b29eef1984440554b4
Svetoslav47 <svetoslav2806@gmail.com>
2024-03-16 02:25:53 +0200

    Update Button.tsx

--- client/Button.tsx
+++ client/Button.tsx
@@ -2,19 +2,19 @@ import React from "react";
 import { TouchableOpacity, Text, StyleSheet, StyleProp, ViewStyle } from "react-native";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function Button({text, onPress, icon, size, color, stylesProp}: 
+export default function Button({ text, onPress, icon, size, color, stylesProp }:
     {
-        text: string, 
-        onPress: () => void, 
-        icon: keyof typeof MaterialCommunityIcons.glyphMap, 
+        text: string,
+        onPress: () => void,
+        icon: keyof typeof MaterialCommunityIcons.glyphMap,
         size: number | undefined,
         color: string,
         stylesProp?: StyleProp<ViewStyle>
     }
-    ) {
+) {
     return (
         <TouchableOpacity onPress={onPress} style={StyleSheet.compose(styles.button, stylesProp)}>
-            <Text style={styles.text}>{text}</Text>
+            {text && <Text style={styles.text}>{text}</Text>}
             <MaterialCommunityIcons name={icon} size={size} color={color} />
         </TouchableOpacity>
     );
@@ -32,9 +32,6 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 20,
         marginLeft: 8,
-        textAlign: 'center',
-        alignItems: 'center',
-        justifyContent: 'center',
         // Add styling for your text
     },
 });
