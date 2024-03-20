308e619996ce753129a983f76c2c801d4554b954
Svetoslav47 <svetoslav2806@gmail.com>
2024-03-21 00:01:41 +0200

    fixed types conflict

--- client/Types.ts
+++ client/Types.ts
@@ -1,5 +1,5 @@
 export type figureData = {
-    vertices: { [key: string]: number[] },
-    edges: string[][],
+    vertices: { [key: string]: [number, number, number] },
+    edges: Array<[string, string]>
     solution: Array<string>
 }
