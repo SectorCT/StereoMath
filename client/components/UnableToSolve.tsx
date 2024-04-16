f6c859d9edb73b31979d16c091eb5d9536aedd7a
Svetoslav47 <svetoslav2806@gmail.com>
2024-04-16 23:30:23 +0300

    loading screen and unable to solve are separate components

---
+++ client/components/UnableToSolve.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Text, Image, StyleSheet, Dimensions } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StackNavigationProp } from "@react-navigation/stack";
+import Button from "../components/Button";
+
+export default function UnableToSolve({ navigation} : { navigation: StackNavigationProp<any>}) {
+    return (
+        <LinearGradient
+          colors={["#bde0fe", "#6685c4", "#445f96"]}
+          style={styles.loading}
+        >
+          <Image
+            style={styles.image}
+            source={require("../../assets/unableToSolve.png")}
+          />
+          <Text style={styles.waitingText}>Unable To Solve</Text>
+          <Button
+            color="white"
+            text="Retry"
+            onPress={() => navigation.goBack()}
+            icon="keyboard-backspace"
+            size={24}
+            stylesProp={styles.retryBtn}
+          />
+        </LinearGradient>
+    );
+}
+
+const styles = StyleSheet.create({
+    loading: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: Dimensions.get("screen").height,
+        marginTop: 0,
+        padding: 0,
+        backgroundColor: "#bde0fe",
+      },
+      image: {
+        height: 180,
+        width: 200,
+      },
+      waitingText: {
+        position: "relative",
+        top: 50,
+        fontSize: 30,
+      },
+      retryBtn: {
+        color: "black",
+        position: "relative",
+        bottom: "-50%",
+        backgroundColor: "#219ebc",
+        borderRadius: 10,
+      },
+});
